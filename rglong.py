b62140b3efab3fbb9d70445dd7dcdb0f1b57ab01
Code Zero <code0@runbox.com>
2024-09-04 10:53:28 +0200

    add longest distance calculation

--- rglong.py
+++ rglong.py
@@ -3,15 +3,25 @@
 
 import math 
 import sys
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
-def Rg(filename):
+
+def RgLong(filename):
     '''
     Given a PDB structure file, calculates the Radius of Gyration (Rg) and
     the longest distance.
     NB: Only takes in account H, C, O, N and S atoms.
     
-    Returns the Rg and Longest values in Angstrom.
+    Returns (values in Angstrom):
+        - the Rg
+        - coordinates for the center of mass
+        - the longest distance between atoms
+        - coordinates for the first atom furthest apart
+        - coordinates for the second atom furthest apart
     '''
+
+
     # Get coordinates and masses for each atom,
     # ignoring lines that do not end with: H, C, O, N, or S
     coord = list()
@@ -35,6 +45,7 @@ def Rg(filename):
             elif line[-1] == 'H':
                 mass.append(1.00794)
         except:
+
             pass
     
     # Determine Center of Mass Coordinates and Total Mass
@@ -47,8 +58,7 @@ def Rg(filename):
     cmx/=tmass
     cmy/=tmass
     cmz/=tmass
-    print("One can check the position in PyMOL with the command:")
-    print("pseudoatom tmpPoint, pos=[", cmx, "," , cmy, "," , cmz, "]")
+    CM = [cmx, cmy, cmz]
         
     # Calculate Radius of Gyration
     rg = 0
@@ -56,14 +66,38 @@ def Rg(filename):
         rg+=m*((i-cmx)**2+(j-cmy)**2+(k-cmz)**2)
     rg/=tmass
     rg=math.sqrt(rg)
-    print("One can check the radius of gyration in PyMOL with the command:")
-    print("pseudoatom tmpPoint, pos=[", cmx, "," , cmy, "," , cmz, "], vdw=", rg)
-    print("Remeber to select tmpPoint and show as Spheres")
 
-    # Determine Longest distance in the structure
-    Longest = 0
+
+    # Determine the Longest distance in the structure
+    
+    # Convert distance vector to a condensed distance matrix, then
+    # converts that matrix  into a redundant distance matrix
+    # (size is 'len(coord) x len(coord)')
+    distances = squareform(pdist(coord))
+    
+    # Find maxmimum distance and index of maximum distance
+    Longest = np.max(distances) 
+    indexLongest = np.argmax(distances)
+    
+    #Find coordinates of atoms at longest distance
+    indexLrow, indexLcol = np.unravel_index(indexLongest, distances.shape)
+  
      
-    return(round(rg, 1 ), Longest)
+    return(round(rg, 1), CM, round(Longest, 1), coord[indexLrow], coord[indexLcol])
+
+
 
 if __name__ == '__main__':
-    print('Rg = {}'.format(Rg(sys.argv[1])))
+    Rg,CM,Long,point1,point2 = RgLong(sys.argv[1])
+    print("Radius of Gyration is ", Rg)
+    print("Longest distance is ", Long)
+    print()
+    print("One can check the radius of gyration in PyMOL with the command:")
+    print("pseudoatom tmpPointCM, pos=", CM, ", vdw=", Rg)
+    print("Remeber to select tmpPoint and show as Spheres and maybe 'set sphere_transparency=0.5' ")
+    print()
+    print("One can get the distance in PyMOL with the commands:")
+    print("pseudoatom tmpPoint1, pos=", point1)
+    print("pseudoatom tmpPoint2, pos=", point2)
+    print("dist distance1, tmpPoint1, tmpPoint2")
+    print()
